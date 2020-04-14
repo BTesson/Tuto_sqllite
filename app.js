a291555b30573df65dded89845f2cd248287c193
BTesson <50833463+BTesson@users.noreply.github.com>
2020-04-14 10:27:50 +0200

    finished crud

--- app.js
+++ app.js
@@ -3,23 +3,43 @@
  */
 const express = require('express');
 const books = require('./books');
+const tablebooks = require('./tableBooks');
 
 let app = express();
+tablebooks.modules.createData();
 
 app
     .get('/', (request, response) => {
         response.setHeader('Content-type', 'application/json; charset=utf-8');
-        response.send('{"result": "Hello"}');
+        response.send('{"title": "Index", "content": "Hello"}');
     })
     .get('/about', (request, response) => {
         response.setHeader('Content-type', 'application/json; charset=utf-8');
-        response.send('{"result": "Hello"}');
+        response.send('{"title": "About", "content": "About"}');
     })
     .get('/data', (request, response) => {
         response.setHeader('Content-type', 'application/json; charset=utf-8');
-        response.send('{"result": "Hello"}');
+        response.send('{"title": "Data", "content": "Data"}');
     })
     .get('/books', (request, response) => {
+        response.setHeader('Content-type', 'application/json; charset=utf-8');
+        books.modules.selectAll(response);
+    })
+    .get('/books/:id', (request, response) => {
+        response.setHeader('Content-type', 'application/json; charset=utf-8');
         books.modules.select(request, response);
+    })
+    .get('/create', (request, response) => {
+        response.setHeader('Content-type', 'application/json; charset=utf-8');
+        response.send('{"title": "Form", content: "{"book_id": "", "book_author": "", "book_comment": ""}"}'); 
+    })
+    .post('/create', (request, response) => {
+        books.modules.create(request, response);
+    })
+    .post('/books/:id/update', (request, response) => {
+        books.modules.update(request, response);
+    })
+    .post('/books/:id/delete', (request, response) => {
+        books.modules.delete(request, response);
     })
     .listen(8080);
--- books.js
+++ books.js
@@ -4,28 +4,106 @@
 const sqllite = require('sqlite3').verbose();
 var path = require('path');
 
-const db_name = path.join(__dirname, "data", "apptest.db");
+const path_db = path.join(__dirname, "apptest.db");
 
-
-const select = (request, response) => {
-        console.log("la");
-        const db = new sqllite.Database(db_name, (err) => {
+const selectAll = (response) => {
+        const db = new sqllite.Database(path_db, (err) => {
             if(err){
                 return console.log(err.message);
             }
             console.log("Connexion réussi à la base de données 'apptest.db'");
         });
-        const sql = 'SELECT * FROM Books ORDERBY Title;';
+        const sql = 'SELECT * FROM Books ORDER BY Title;';
         //sql => requete sql
         //[] => tableau de variable nécéssaires à la requete
         db.all(sql, [], (err, rows) => {
             if(err){
                 return console.log(err.message);
             }
-            response.send({"model": rows});
+            response.send('{"title": "Books", "content": ' + JSON.stringify(rows) + '}');
         });
     }
 
+const select = (request, response) => {
+    const db = new sqllite.Database(path_db, (err) => {
+        if(err){
+            return console.log(err.message);
+        }
+        console.log("Connexion réussi à la base de données 'apptest.db'");
+    });
+    const sql = 'SELECT * FROM Books WHERE Book_ID = ?;';
+    //sql => requete sql
+    //[] => tableau de variable nécéssaires à la requete
+    db.get(sql, request.params.id, (err, rows) => {
+        if(err){
+            return console.log(err.message);
+        }
+        response.send('{"title": "Books", "content": ' + JSON.stringify(rows) + '}');
+    });
+}
+
+const create = (request, response) => {
+    const db = new sqllite.Database(path_db, (err) => {
+        if(err){
+            return console.log(err.message);
+        }
+        console.log("Connexion réussi à la base de données 'apptest.db'");
+    });
+    //let book = [request.body.book_title, request.body.book_author, request.body.book_comment];
+    let book = ["Mrs. Bridge", "Evan S. Connell", "Premier de la série"]
+    const sql = 'INSERT INTO Books (Title, Author, Comments) Values (?, ?, ?);';
+    //sql => requete sql
+    //[] => tableau de variable nécéssaires à la requete
+    db.run(sql, book, (err, rows) => {
+        if(err){
+            return console.log(err.message);
+        }
+        response.redirect('/books');
+    });
+}
+
+const update = (request, response) => {
+    const db = new sqllite.Database(path_db, (err) => {
+        if(err){
+            return console.log(err.message);
+        }
+        console.log("Connexion réussi à la base de données 'apptest.db'");
+    });
+    //let book = [request.body.book_title, request.body.book_author, request.body.book_comment, id];
+    let book = ["Test", "Moi", "Réussi", request.params.id];
+    const sql = 'UPDATE Books SET Title = ?, Author = ?, Comments = ? WHERE (Book_ID = ?);';
+    //sql => requete sql
+    //[] => tableau de variable nécéssaires à la requete
+    db.run(sql, book, (err, rows) => {
+        if(err){
+            return console.log(err.message);
+        }
+        response.redirect(200, '/books/' + request.params.id);
+    });
+}
+
+const deletee = (request, response) => {
+    const db = new sqllite.Database(path_db, (err) => {
+        if(err){
+            return console.log(err.message);
+        }
+        console.log("Connexion réussi à la base de données 'apptest.db'");
+    });
+    const sql = 'DELETE FROM Books WHERE Book_ID = ?;';
+    //sql => requete sql
+    //[] => tableau de variable nécéssaires à la requete
+    db.run(sql, request.params.id, (err, rows) => {
+        if(err){
+            return console.log(err.message);
+        }
+        response.redirect('/books');
+    });
+}
+
 exports.modules = {
     select: select,
+    selectAll: selectAll,
+    create: create,
+    update: update,
+    delete: deletee,
 };
